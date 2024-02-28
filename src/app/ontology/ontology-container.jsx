fc2c2dc18151b0e55efc9d98f42d415d1a4daf87
John Sohrawardi <saniat.s@mail.rit.edu>
2024-02-28 16:41:52 -0500

    working on adding redux

--- src/app/ontology/ontology-container.jsx
+++ src/app/ontology/ontology-container.jsx
@@ -1,23 +1,21 @@
 import React from 'react';
-import data from '../../../data/ontology.json';
-import Option from '../shells/option-block';
+import Option from './option-block';
 
-const OntologyContainer = () => {
-
+const OntologyContainer = ({ data }) => {
     const topLevel = data['start'].children;
-    const topLevelKeys = Object.keys(topLevel)
+    const topLevelKeys = Object.keys(topLevel);
 
     function objectToArray(obj) {
         return Object.keys(obj).map((key, index) => {
             return obj[key];
         });
     }
+
     return topLevelKeys.map((key, index) => {
         return (
             <Option key={index} title={key} data={objectToArray(topLevel[key].children)} />
-        )
+        );
     });
-
 };
 
 export default OntologyContainer;
