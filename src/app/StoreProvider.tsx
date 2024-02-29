8ebcb1b7515a6219e06bc7d4bcf0285f7777c93d
John Sohrawardi <saniat.s@gmail.com>
2024-02-28 23:24:32 -0500

    wip: setting up redux store

--- src/app/StoreProvider.tsx
+++ src/app/StoreProvider.tsx
@@ -2,17 +2,21 @@
 import { useRef } from 'react'
 import { Provider } from 'react-redux'
 import { makeStore, AppStore } from '../lib/store'
+import { initializeOntology } from '../lib/features/ontology/ontologySlice'
 
+// open json file and initialize the state
+const nodes = require("../../data/nodes.json");
+const edges = require("../../data/edges.json");
 
 export default function StoreProvider({
     children
 }: {
     children: React.ReactNode
 }) {
-    const storeRef = useRef<AppStore>()
+    const storeRef = useRef<AppStore | null>(null)
     if (!storeRef.current) {
-        // Create the store instance the first time this renders
         storeRef.current = makeStore()
+        storeRef.current.dispatch(initializeOntology({ nodes, edges }))
     }
 
     return <Provider store={storeRef.current}>{children}</Provider>
--- src/lib/features/ontology/ontologySlice.ts
+++ src/lib/features/ontology/ontologySlice.ts
@@ -1,3 +1,52 @@
-import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { PayloadAction, createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import generateLinearOntology from '../../../app/helpers/generate-linear-ontology';
+import processOntology from '../../../app/helpers/process-ontology';
+
+// const nodes = require("../../data/nodes.json");
+// const edges = require("../../data/edges.json");
+// const ontology = require("../../data/ontology.json");
+
+export interface OntologyState {
+    filter: Object;
+    tree: Object;
+}
+
+// initialize the state from the json file
+const initialState = {
+    filter: {},
+    tree: {}
+};
+
+const _updateFilter = (state: any, action: PayloadAction<any>) => {
+    console.log(action.payload)
+    state.filter = action.payload;
+    const ontologyItems = Object.keys(state.filter);
+}
 
 
+export const ontologySlice = createSlice({
+    name: "ontology",
+    initialState,
+    reducers: {
+        initializeOntology: (state, action) => {
+            state.filter = generateLinearOntology(action.payload.nodes, action.payload.edges);
+            state.tree = processOntology(action.payload.nodes, action.payload.edges);
+        },
+        setFilter: (state, action) => {
+            state.filter = action.payload;
+        },
+        updateFilter: {
+            reducer(state, action: PayloadAction<any>) {
+                _updateFilter(state, action);
+            },
+            prepare(payload: any) {
+                return { payload };
+            }
+        }
+    }
+});
+
+export const { setFilter, updateFilter, initializeOntology } = ontologySlice.actions;
+export const selectFilter = (state: OntologyState) => state.filter;
+export const selectTree = (state: OntologyState) => state.tree;
+export default ontologySlice.reducer;
