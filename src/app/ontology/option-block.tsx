f956204f2758526edc685b8aef81da778b52aed7
John Sohrawardi <saniat.s@gmail.com>
2024-03-15 19:06:25 -0400

    added optional selected ontology sorting

--- src/app/ontology/option-block.tsx
+++ src/app/ontology/option-block.tsx
@@ -13,6 +13,16 @@ const OptionBlock: React.FC<OptionBlockProps> = ({ title, nodenames }) => {
     const dispatch = useAppDispatch();
     const filter: OntologyFilter = useAppSelector((state) => state.ontology.filter);
 
+    // sort by state
+    // const sortByState = (a: string, b: string) => {
+    //     if (filter[a].state === "selected") return -1;
+    //     if (filter[b].state === "selected") return 1;
+    //     if (filter[a].state === "available") return -1;
+    //     if (filter[b].state === "available") return 1;
+    //     return 0;
+    // }
+    // nodenames.sort(sortByState);
+
     return (
         <div className={`${title} option-block scroll-shadows`}>
             <h2 className="">{title}</h2>
